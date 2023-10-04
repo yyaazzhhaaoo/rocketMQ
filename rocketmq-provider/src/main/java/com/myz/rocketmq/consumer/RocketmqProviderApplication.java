4cde45df35cc8b90ac44504bec0d36c9053eb59e
meiyazhao <m13102876912@163.com>
2023-10-04 11:31:17 +0800

    springboot工程创建

---
+++ rocketmq-provider/src/main/java/com/myz/rocketmq/consumer/RocketmqProviderApplication.java
@@ -0,0 +1,11 @@
+package com.myz.rocketmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqProviderApplication.class, args);
+    }
+}
